d9910c9c5306a5dd792bc49d27ff4e88e6e34dd9
eli0504167101 <eh4167101@gmail.com.com>
2026-02-04 11:52:48 +0200

    Fix host address for Docker and add Dockerfile

--- app.py
+++ app.py
@@ -12,5 +12,5 @@ def health():
     return jsonify(status = "ok")
 
 
-if __name__ == "__main__":
-   app.run(host="0.0.0.0.", port=5000)
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=5000)
